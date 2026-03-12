9b327da5fc0d56948a3123ee47ffd8fbcf441ecf
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-03-13 00:07:22 +0530

    Project 03

--- 03_color-palette-generator/main.js
+++ 03_color-palette-generator/main.js
@@ -1,27 +1,59 @@
-const input = document.getElementById("itemInput");
-const addBtn = document.getElementById("addBtn");
-const list = document.getElementById("list");
+const formatSelect = document.getElementById("format");
+const toneSelect = document.getElementById("tone");
+const button = document.getElementById("generateBtn");
+const palette = document.getElementById("palette");
 
-addBtn.addEventListener("click" , ()=>{
-   if(input.value === "") {
-    alert("Mat kr lala!")
-    return;
+function randomRGB(tone) {
+  let min = 0;
+  let max = 255;
+
+  if (tone === "light") {
+    min = 150;
+    max = 255;
+  }
+
+  if (tone === "dark") {
+    min = 0;
+    max = 125;
+  }
+
+  const r = Math.floor(Math.random() * (max-min) + min);
+  const g = Math.floor(Math.random() * (max-min) + min);
+  const b = Math.floor(Math.random() * (max-min) + min);
+
+  return {r , g , b}
+}
+
+function rgbToHex(r, g, b) {
+   return "#" + [r,g,b].map((x)=>x.toString(16).padStart(2 , "0")).join("")
+}
+
+function generatePalette() {
+
+   palette.innerHTML = "";
+
+   for(let i =0 ; i<5; i++){
+         const {r , g , b} = randomRGB(toneSelect.value);
+
+         let color;
+
+         if(formatSelect.value === 'hex'){
+            color = rgbToHex(r,g,b)
+         }
+         else{
+            color = `rgb(${r}, ${g}, ${b})`
+         }
+
+         const div = document.createElement("div");
+         div.classList.add("color");
+
+         div.style.background = `rgb(${r}, ${g}, ${b})`;
+         div.textContent = color;
+
+         palette.appendChild(div)
    }
+}
 
-   const li = document.createElement("li");
-   const delBtn = document.createElement("button");
-   delBtn.textContent = "Delete";
-   delBtn.classList.add("delete");
+button.addEventListener("click", generatePalette);
 
-   li.textContent = input.value;
-
-   delBtn.addEventListener("click" , ()=>{
-        li.remove();
-   })
-
-   li.appendChild(delBtn)
-   list.appendChild(li);
-
-   input.value = ""
-
-})
+generatePalette();
